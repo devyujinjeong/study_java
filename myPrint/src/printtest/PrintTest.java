e690d7c917f3499813a383f9507ac23180a96261
devyujinjeong <devyujinjeong887@gmail.com>
2024-07-29 15:22:01 +0900

    myPrint 수정 완료

---
+++ myPrint/src/printtest/PrintTest.java
@@ -0,0 +1,36 @@
+package printtest;
+
+public class PrintTest {
+	public static void main(String[] args) { //main치고 ctrl+enter누르면 자동완성됨
+		
+//		글자 크기 키우기:CTRL++
+//		글자 크기 줄이기:CTRL+-
+		
+//		주석 (ctrl+/)
+//		1. 소스코드의 설명글을 작성할 때
+//		2. 지금 당장 사용하지 않는 코드를 번역하고 싶지 않을 때
+	
+		/*
+		 * 범위 주석(C	TRL+SHIFT+/).해제(CTRL+SHIFT+\)
+		 */
+		
+// 		제어문자
+//		반드시 따옴표 안에서 작성한다.
+		
+//		\n : new line, 줄바꿈, 개행문자
+//		\t : tab, 위 아래 줄 간격 맞춰 띄기
+//      \" : " 표현
+//      \' : ' 표현
+//      \\ : \ 표현
+		
+		//이름을 출력하는 부분
+//		"정유진"을 출력
+		System.out.print("\"정유진\"\n");
+		//나이를 출력하는 부분
+//		23살\ 을 출력함 
+		System.out.print("23살\\");
+		
+		System.out.println("안녕");
+		System.out.println("하세요");
+	}
+}
