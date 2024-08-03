a6a8f7eff3dfa8da34b48c55634ae26c45703af5
devyujinjeong <devyujinjeong887@gmail.com>
2024-08-03 17:53:43 +0900

    API 테스트, 현재 외부 API가 삭제되어 있음

--- myApi/src/apiTest/RandomTest.java
+++ myApi/src/apiTest/RandomTest.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomTest {
 	public static void main(String[] args) {
-//		시간을 기준점으로!!
+//		랜덤은 내장되어 있는 API
 		Random random = new Random();
 //		nextInt라는 거 안에 있는 수의 나머지가 나오게 됨
 //		즉, 이 경우에는 0~2까지 숫자 중에서 랜덤으로 숫자가 나옴
